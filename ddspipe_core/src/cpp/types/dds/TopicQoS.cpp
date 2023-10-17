741dd687932573f7b7a35a6c8e317d2cc64cb2cd
tempate <danieldiaz@eprosima.com>
2023-10-17 09:07:29 +0200

    Fix compilation in Ubuntu 20.04 debug
    Signed-off-by: tempate <danieldiaz@eprosima.com>

--- ddspipe_core/src/cpp/types/dds/TopicQoS.cpp
+++ ddspipe_core/src/cpp/types/dds/TopicQoS.cpp
@@ -25,27 +25,32 @@ namespace ddspipe {
 namespace core {
 namespace types {
 
-std::atomic<HistoryDepthType> TopicQoS::default_history_depth{HISTORY_DEPTH_DEFAULT};
-std::atomic<float> TopicQoS::default_max_tx_rate{MAX_TX_RATE_DEFAULT};
-std::atomic<float> TopicQoS::default_max_rx_rate{MAX_RX_RATE_DEFAULT};
-std::atomic<unsigned int> TopicQoS::default_downsampling{DOWNSAMPLING_DEFAULT};
+std::atomic<DurabilityKind> TopicQoS::default_durability_qos{DEFAULT_DURABILITY_QOS};
+std::atomic<ReliabilityKind> TopicQoS::default_reliability_qos{DEFAULT_RELIABILITY_QOS};
+std::atomic<OwnershipQosPolicyKind> TopicQoS::default_ownership_qos{DEFAULT_OWNERSHIP_QOS};
+std::atomic<bool> TopicQoS::default_use_partitions{DEFAULT_USE_PARTITIONS};
+std::atomic<bool> TopicQoS::default_keyed{DEFAULT_KEYED};
+std::atomic<HistoryDepthType> TopicQoS::default_history_depth{DEFAULT_HISTORY_DEPTH};
+std::atomic<float> TopicQoS::default_max_tx_rate{DEFAULT_MAX_TX_RATE};
+std::atomic<float> TopicQoS::default_max_rx_rate{DEFAULT_MAX_RX_RATE};
+std::atomic<unsigned int> TopicQoS::default_downsampling{DEFAULT_DOWNSAMPLING};
 
 TopicQoS::TopicQoS()
 {
     // Set the default durability
-    durability_qos.set_value(DURABILITY_QOS_DEFAULT, utils::FuzzyLevelValues::fuzzy_level_default);
+    durability_qos.set_value(default_durability_qos, utils::FuzzyLevelValues::fuzzy_level_default);
 
     // Set the default reliabitliy
-    reliability_qos.set_value(RELIABILITY_QOS_DEFAULT, utils::FuzzyLevelValues::fuzzy_level_default);
+    reliability_qos.set_value(default_reliability_qos, utils::FuzzyLevelValues::fuzzy_level_default);
 
     // Set the default ownership
-    ownership_qos.set_value(OWNERSHIP_QOS_DEFAULT, utils::FuzzyLevelValues::fuzzy_level_default);
+    ownership_qos.set_value(default_ownership_qos, utils::FuzzyLevelValues::fuzzy_level_default);
 
     // Set whether by default the Topic should use partitions
-    use_partitions.set_value(USE_PARTITIONS_DEFAULT, utils::FuzzyLevelValues::fuzzy_level_default);
+    use_partitions.set_value(default_use_partitions, utils::FuzzyLevelValues::fuzzy_level_default);
 
     // Set whether by default the Topic has a key
-    keyed.set_value(KEYED_DEFAULT, utils::FuzzyLevelValues::fuzzy_level_default);
+    keyed.set_value(default_keyed, utils::FuzzyLevelValues::fuzzy_level_default);
 
     // Set the default history depth
     history_depth.set_value(default_history_depth, utils::FuzzyLevelValues::fuzzy_level_default);
