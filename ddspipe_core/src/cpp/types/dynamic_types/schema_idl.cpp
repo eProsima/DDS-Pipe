9bbe5bd010f0398d400ef6f668b258278fcc98af
LuciaEchevarria99 <110391668+LuciaEchevarria99@users.noreply.github.com>
2023-12-19 17:56:59 +0100

    Fix breaks for TSAN tests (#80)
    * Fix breaks for TSAN tests
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>
    
    * Remove blank spaces
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>
    
    ---------
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>

--- ddspipe_core/src/cpp/types/dynamic_types/schema_idl.cpp
+++ ddspipe_core/src/cpp/types/dynamic_types/schema_idl.cpp
@@ -264,7 +264,6 @@ utils::TreeNode<TreeNodeType> generate_dyn_type_tree(
             }
             return parent;
         }
-        break;
 
         case fastrtps::types::TK_ARRAY:
         case fastrtps::types::TK_SEQUENCE:
@@ -280,11 +279,9 @@ utils::TreeNode<TreeNodeType> generate_dyn_type_tree(
 
             return container;
         }
-        break;
 
         default:
             return utils::TreeNode<TreeNodeType>(member_name, type_kind_to_str(type), type);
-            break;
     }
 }
 
