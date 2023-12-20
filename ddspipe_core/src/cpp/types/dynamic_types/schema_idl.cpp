dbe89325eeb9798a6129fcc69a7df964d1651086
Raul Sanchez-Mateos <raul@eprosima.com>
2023-12-20 07:39:47 +0100

    Undo tsan fix
    Signed-off-by: Raul Sanchez-Mateos <raul@eprosima.com>

--- ddspipe_core/src/cpp/types/dynamic_types/schema_idl.cpp
+++ ddspipe_core/src/cpp/types/dynamic_types/schema_idl.cpp
@@ -281,9 +281,7 @@ utils::TreeNode<TreeNodeType> generate_dyn_type_tree(
         }
 
         default:
-        {
             return utils::TreeNode<TreeNodeType>(member_name, type_kind_to_str(type), type);
-        }
     }
 }
 
