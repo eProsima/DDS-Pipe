d8ad3899eb7f23e5cb39cc04ace71c0cfaea3e32
tempate <danieldiaz@eprosima.com>
2024-02-06 11:41:50 +0100

    Include the producers to access the operator<<
    Signed-off-by: tempate <danieldiaz@eprosima.com>

--- ddspipe_core/include/ddspipe_core/monitoring/consumers/StdoutMonitorConsumer.hpp
+++ ddspipe_core/include/ddspipe_core/monitoring/consumers/StdoutMonitorConsumer.hpp
@@ -17,6 +17,8 @@
 
 #include <ddspipe_core/configuration/MonitorConfiguration.hpp>
 #include <ddspipe_core/monitoring/consumers/IMonitorConsumer.hpp>
+#include <ddspipe_core/monitoring/producers/StatusMonitorProducer.hpp>
+#include <ddspipe_core/monitoring/producers/TopicsMonitorProducer.hpp>
 
 namespace eprosima {
 namespace ddspipe {
