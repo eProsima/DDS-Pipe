b3bd01ff98e3b5c49bf0b8a5a8db04a77874b853
Lucia Echevarria <luciaechevarria@eprosima.com>
2024-02-07 15:54:21 +0100

    Add LogConfig comment
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>

--- ddspipe_core/include/ddspipe_core/configuration/DdsPipeConfiguration.hpp
+++ ddspipe_core/include/ddspipe_core/configuration/DdsPipeConfiguration.hpp
@@ -123,7 +123,7 @@ struct DdsPipeConfiguration : public IConfiguration
     //! The type of the entity whose discovery should trigger the discovery callbacks.
     DiscoveryTrigger discovery_trigger = DiscoveryTrigger::READER;
 
-    //
+    // Logging Configuration.
     utils::LogConfiguration log_configuration{};
 };
 
