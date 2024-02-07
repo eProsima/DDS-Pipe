e366c602a7b7e52318d09108237cea4076ff5855
tempate <danieldiaz@eprosima.com>
2024-02-07 16:05:14 +0100

    Replace ResourceEvent and TimedEvent with PeriodicEventHandler
    Signed-off-by: tempate <danieldiaz@eprosima.com>

--- ddspipe_core/src/cpp/monitoring/Monitor.cpp
+++ ddspipe_core/src/cpp/monitoring/Monitor.cpp
@@ -20,42 +20,15 @@ namespace eprosima {
 namespace ddspipe {
 namespace core {
 
-Monitor::Monitor()
-{
-    fastdds::rtps::ThreadSettings thread_config;
-    event_handler_.init_thread(thread_config, "ddspipe.monitor%u");
-}
-
-Monitor::~Monitor()
-{
-    event_handler_.stop_thread();
-    clear_producers();
-}
-
 void Monitor::register_producer(
         IMonitorProducer* producer)
 {
-    std::unique_lock<std::mutex> lock(mutex_);
+    std::function<void()> periodic_callback = [producer]()
+            {
+                producer->consume();
+            };
 
-    events_.emplace_back(std::make_unique<fastrtps::rtps::TimedEvent>(
-        event_handler_,
-        [producer]() -> bool
-        {
-            producer->consume();
-
-            // The return value must be true to reschedule the event.
-            return true;
-        },
-        producer->period));
-
-    // Start the timer
-    events_.back()->restart_timer();
-}
-
-void Monitor::clear_producers()
-{
-    std::unique_lock<std::mutex> lock(mutex_);
-    events_.clear();
+    events_.emplace_back(std::make_unique<utils::event::PeriodicEventHandler>(periodic_callback, producer->period));
 }
 
 } //namespace core
