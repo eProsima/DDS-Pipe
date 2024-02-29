422b54f3661f9eb7f807c9a1fa1b6b966279e328
tempate <danieldiaz@eprosima.com>
2024-02-29 13:04:25 +0100

    Test that the logs get published correctly
    Signed-off-by: tempate <danieldiaz@eprosima.com>

---
+++ ddspipe_core/test/unittest/logging/DdsLogConsumerTest.cpp
@@ -0,0 +1,211 @@
+// Copyright 2024 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <chrono>
+#include <thread>
+
+#include <cpp_utils/testing/gtest_aux.hpp>
+#include <gtest/gtest.h>
+
+#include <fastdds/dds/domain/DomainParticipant.hpp>
+#include <fastdds/dds/domain/DomainParticipantFactory.hpp>
+#include <fastdds/dds/subscriber/qos/DataReaderQos.hpp>
+#include <fastdds/dds/subscriber/DataReader.hpp>
+#include <fastdds/dds/subscriber/SampleInfo.hpp>
+#include <fastdds/dds/subscriber/Subscriber.hpp>
+#include <fastdds/dds/topic/qos/TopicQos.hpp>
+#include <fastdds/dds/topic/Topic.hpp>
+#include <fastdds/dds/topic/TypeSupport.hpp>
+
+#include <cpp_utils/Log.hpp>
+#include <cpp_utils/logging/StdLogConsumer.hpp>
+
+#include <ddspipe_core/configuration/DdsLogConfiguration.hpp>
+#include <ddspipe_core/logging/DdsLogConsumer.hpp>
+#include <ddspipe_core/types/dds/DomainId.hpp>
+
+#if FASTRTPS_VERSION_MAJOR < 2 || (FASTRTPS_VERSION_MAJOR == 2 && FASTRTPS_VERSION_MINOR < 13)
+    #include <ddspipe_core/types/logging/v1/LogEntry.h>
+    #include <ddspipe_core/types/logging/v1/LogEntryPubSubTypes.h>
+    #include <ddspipe_core/types/logging/v1/LogEntryTypeObject.h>
+#else
+    #include <ddspipe_core/types/logging/v2/LogEntry.h>
+    #include <ddspipe_core/types/logging/v2/LogEntryPubSubTypes.h>
+    #include <ddspipe_core/types/logging/v2/LogEntryTypeObject.h>
+#endif // if FASTRTPS_VERSION_MAJOR < 2 || (FASTRTPS_VERSION_MAJOR == 2 && FASTRTPS_VERSION_MINOR < 13)
+
+using namespace eprosima;
+using namespace eprosima::fastdds::dds;
+
+class DdsLogConsumerTest : public testing::Test
+{
+public:
+
+    void SetUp() override
+    {
+        // Create the participant
+        DomainParticipantQos pqos;
+        pqos.name("DdsLogParticipantTest");
+
+        participant_ = DomainParticipantFactory::get_instance()->create_participant(DOMAIN, pqos);
+
+        ASSERT_NE(participant_, nullptr);
+
+        // Register the type
+        TypeSupport type(new LogEntryPubSubType());
+        type.register_type(participant_);
+
+        // Create the subscriber
+        Subscriber* subscriber = participant_->create_subscriber(SUBSCRIBER_QOS_DEFAULT, nullptr);
+
+        ASSERT_NE(subscriber, nullptr);
+
+        // Create the topic
+        TopicQos tqos = TOPIC_QOS_DEFAULT;
+        tqos.reliability().kind = BEST_EFFORT_RELIABILITY_QOS;
+        tqos.durability().kind = VOLATILE_DURABILITY_QOS;
+
+        Topic* topic = participant_->create_topic(TOPIC_NAME, type.get_type_name(), tqos);
+
+        ASSERT_NE(topic, nullptr);
+
+        // Create the reader
+        DataReaderQos rqos = DATAREADER_QOS_DEFAULT;
+
+        rqos.data_sharing().automatic();
+        rqos.reliability().kind = BEST_EFFORT_RELIABILITY_QOS;
+        rqos.durability().kind = VOLATILE_DURABILITY_QOS;
+
+        reader_ = subscriber->create_datareader(topic, rqos);
+
+        ASSERT_NE(reader_, nullptr);
+    }
+
+    void TearDown() override
+    {
+        if (nullptr != participant_)
+        {
+            participant_->delete_contained_entities();
+            DomainParticipantFactory::get_instance()->delete_participant(participant_);
+        }
+    }
+
+protected:
+
+    const ddspipe::core::types::DomainIdType DOMAIN{84};
+    const std::string TOPIC_NAME{"DdsLogsTopic"};
+
+    DomainParticipant* participant_ = nullptr;
+    DataReader* reader_ = nullptr;
+};
+
+/**
+ * Test that the DdsLogConsumer publishes LogEntries correctly.
+ *
+ * CASES:
+ * - check that the DdsLogConsumer publishes logInfos correctly.
+ * - check that the DdsLogConsumer publishes logWarnings correctly.
+ * - check that the DdsLogConsumer publishes logErrors correctly.
+ */
+TEST_F(DdsLogConsumerTest, is_publishing_log_entries_correctly)
+{
+    // Configure the Log to publish info messages on the topic
+    ddspipe::core::DdsLogConfiguration log_configuration;
+    log_configuration.stdout_enable = false;
+    log_configuration.publish.enable = true;
+    log_configuration.publish.domain = DOMAIN;
+    log_configuration.publish.topic_name = TOPIC_NAME;
+
+    // Filter out every LogInfo except ours
+    log_configuration.verbosity = utils::VerbosityKind::Info;
+    log_configuration.filter[utils::VerbosityKind::Info].set_value("DDSPIPE_TEST");
+    log_configuration.filter[utils::VerbosityKind::Warning].set_value("DDSPIPE_TEST");
+    log_configuration.filter[utils::VerbosityKind::Error].set_value("DDSPIPE_TEST");
+
+    utils::Log::ClearConsumers();
+    utils::Log::SetVerbosity(log_configuration.verbosity);
+
+    // Create the DdsLogConsumer
+    auto consumer = std::make_unique<ddspipe::core::DdsLogConsumer>(log_configuration);
+
+    // Add DdsPipeTest specific events
+    const int32_t LOG_CONSUMER_TEST = 0x10010001;
+    consumer->add_event("LOG_CONSUMER_TEST", LOG_CONSUMER_TEST);
+
+    // Register the consumer
+    utils::Log::RegisterConsumer(std::move(consumer));
+
+    // Wait for the publisher and the subscriber to match
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    // Check the information in the LogEntry published by the Log
+    LogEntry entry;
+    SampleInfo info;
+
+    // Log an info message
+    {
+        logInfo(DDSPIPE_TEST, "LOG_CONSUMER_TEST | You only live once.");
+
+        // Wait for the subscriber to receive the message
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        ASSERT_EQ(reader_->take_next_sample(&entry, &info), ReturnCode_t::RETCODE_OK);
+        ASSERT_EQ(info.instance_state, ALIVE_INSTANCE_STATE);
+
+        ASSERT_EQ(entry.event(), LOG_CONSUMER_TEST);
+        ASSERT_EQ(entry.kind(), Kind::Info);
+        ASSERT_EQ(entry.category(), "DDSPIPE_TEST");
+        ASSERT_EQ(entry.message(), "LOG_CONSUMER_TEST | You only live once.");
+    }
+
+    // Log a warning message
+    {
+        logWarning(DDSPIPE_TEST, "LOG_CONSUMER_TEST | You only live once.");
+
+        // Wait for the subscriber to receive the message
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        ASSERT_EQ(reader_->take_next_sample(&entry, &info), ReturnCode_t::RETCODE_OK);
+        ASSERT_EQ(info.instance_state, ALIVE_INSTANCE_STATE);
+
+        ASSERT_EQ(entry.event(), LOG_CONSUMER_TEST);
+        ASSERT_EQ(entry.kind(), Kind::Warning);
+        ASSERT_EQ(entry.category(), "DDSPIPE_TEST");
+        ASSERT_EQ(entry.message(), "LOG_CONSUMER_TEST | You only live once.");
+    }
+
+    // Log an error message
+    {
+        logError(DDSPIPE_TEST, "LOG_CONSUMER_TEST | You only live once.");
+
+        // Wait for the subscriber to receive the message
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        ASSERT_EQ(reader_->take_next_sample(&entry, &info), ReturnCode_t::RETCODE_OK);
+        ASSERT_EQ(info.instance_state, ALIVE_INSTANCE_STATE);
+
+        ASSERT_EQ(entry.event(), LOG_CONSUMER_TEST);
+        ASSERT_EQ(entry.kind(), Kind::Error);
+        ASSERT_EQ(entry.category(), "DDSPIPE_TEST");
+        ASSERT_EQ(entry.message(), "LOG_CONSUMER_TEST | You only live once.");
+    }
+}
+
+int main(
+        int argc,
+        char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
