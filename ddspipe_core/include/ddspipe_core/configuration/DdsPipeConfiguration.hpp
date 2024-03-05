6c8afa2ad2f84b467c4aa9d50554c1bc4f68f5fd
LuciaEchevarria99 <110391668+LuciaEchevarria99@users.noreply.github.com>
2024-03-05 15:04:34 +0100

    Configure the Log through YAML (#86)
    * Add YAML configuration of logging
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>
    
    * Add LogConfiguration to set in YAML
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>
    
    * Apply suggested changes
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>
    
    * Correct fuzzy level of verbosity and filter to SET when configured by YAML
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>
    
    * Move LogConfiguration to dev-utils and make suggested changes
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>
    
    * Correct uncrustify
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>
    
    * Eliminate unnecessary namespace and get_value_in_tag
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>
    
    * Add LogConfig comment
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>
    
    * Add CommandlineArgs struct
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>
    
    * Apply suggested changes and rewrite better comments
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>
    
    * Add ddspipe_yaml Log Configuration tests and apply suggested changes
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>
    
    * Apply suggested changes
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>
    
    * Delect on_incompatible_type from this branch
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>
    
    * Change YamlReaderLogConfig tests descriptions
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>
    
    * Change is_valid method to virtual
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>
    
    * Change LogConfiguration tests names and delete unnecessary file
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>
    
    * Correct bug in tests
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>
    
    * Correct uncrustify
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>
    
    ---------
    
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>

--- ddspipe_core/include/ddspipe_core/configuration/DdsPipeConfiguration.hpp
+++ ddspipe_core/include/ddspipe_core/configuration/DdsPipeConfiguration.hpp
@@ -18,6 +18,7 @@
 #include <set>
 
 #include <cpp_utils/Formatter.hpp>
+#include <cpp_utils/logging/LogConfiguration.hpp>
 #include <cpp_utils/macros/custom_enumeration.hpp>
 
 #include <ddspipe_core/configuration/IConfiguration.hpp>
@@ -121,6 +122,9 @@ struct DdsPipeConfiguration : public IConfiguration
 
     //! The type of the entity whose discovery should trigger the discovery callbacks.
     DiscoveryTrigger discovery_trigger = DiscoveryTrigger::READER;
+
+    // Configuration of the CustomStdLogConsumer.
+    utils::LogConfiguration log_configuration{};
 };
 
 } /* namespace core */
