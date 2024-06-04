c5b24fb297431627616686b9d04d9659e6fe0737
tempate <danieldiaz@eprosima.com>
2024-06-04 15:49:06 +0200

    Remove virtual keyword from methods that override
    Signed-off-by: tempate <danieldiaz@eprosima.com>

--- ddspipe_participants/include/ddspipe_participants/efficiency/cache_change/CacheChangePool.hpp
+++ ddspipe_participants/include/ddspipe_participants/efficiency/cache_change/CacheChangePool.hpp
@@ -46,20 +46,20 @@ public:
             utils::PoolConfiguration configuration);
 
     //! Call UnboundedPool::reserve
-    virtual bool reserve_cache(
+    bool reserve_cache(
             fastrtps::rtps::CacheChange_t*& cache_change) override;
 
     //! Call UnboundedPool::release
-    virtual bool release_cache(
+    bool release_cache(
             fastrtps::rtps::CacheChange_t* cache_change) override;
 
 protected:
 
     //! Override the UnboundedPool::create_element method to create a RouterCacheChange object.
-    virtual fastrtps::rtps::CacheChange_t* new_element_() override;
+    fastrtps::rtps::CacheChange_t* new_element_() override;
 
-    //! Override the IPool::reset_element_ method to reset the CacheChange as a new obj.
-    virtual void reset_element_(
+    //! Override the IPool::reset_element_ method to reset the CacheChange as a new object.
+    void reset_element_(
             fastrtps::rtps::CacheChange_t* change) override;
 };
 
